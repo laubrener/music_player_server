e9ba1bf125be539a38d3e3730df1201e2fcb4840
laubrener <79546818+laubrener@users.noreply.github.com>
2023-12-08 12:18:08 -0300

    This reverts commit

--- index.js
+++ index.js
@@ -2,28 +2,15 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-// db config
-require('./database/config').dbConnection();
-
-// app express
 const app = express();
 
-// lectura y parseo del body
-app.use(express.json());
-
-
-// node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 
-// path público
+
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
-
-// mis rutas
-app.use('/api/login', require('./routes/auth'));
-
 
 server.listen(process.env.PORT, (err) => {
     if(err) throw new Error(err);
